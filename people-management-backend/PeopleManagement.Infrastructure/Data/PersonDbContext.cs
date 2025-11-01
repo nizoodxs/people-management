75490b9a8a05e90a4ced10fdf4ade419ba0729c5
Nischal Subedi <subedi275@gmail.com>
2025-11-01 13:46:44 +0000

    Added EF core configuration

---
+++ people-management-backend/PeopleManagement.Infrastructure/Data/PersonDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleManagement.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManagement.Infrastructure.Data
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options): base(options) { }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasMany(p => p.Addresses)
+                .WithOne(a => a.Person)
+                .HasForeignKey(a => a.PersonId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
